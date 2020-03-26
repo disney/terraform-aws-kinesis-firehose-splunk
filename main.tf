a40114c70e9903b1170ee30040f15bc4dc11a89c
Kevin Kuszyk <kevin@kevinkuszyk.com>
2020-03-26 17:59:24 +0000

    Upgrade to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,62 +1,56 @@
 # Kenisis firehose stream
 # Record Transformation Required, called "processing_configuration" in Terraform
 resource "aws_kinesis_firehose_delivery_stream" "kinesis_firehose" {
-  name        = "${ var.firehose_name }"
+  name        = var.firehose_name
   destination = "splunk"
 
   s3_configuration {
-    role_arn           = "${ aws_iam_role.kinesis_firehose.arn }"
-    prefix             = "${ var.s3_prefix }"
-    bucket_arn         = "${ aws_s3_bucket.kinesis_firehose_s3_bucket.arn }"
-    buffer_size        = "${ var.kinesis_firehose_buffer }"
-    buffer_interval    = "${ var.kinesis_firehose_buffer_interval }"
-    compression_format = "${ var.s3_compression_format }"
+    role_arn           = aws_iam_role.kinesis_firehose.arn
+    prefix             = var.s3_prefix
+    bucket_arn         = aws_s3_bucket.kinesis_firehose_s3_bucket.arn
+    buffer_size        = var.kinesis_firehose_buffer
+    buffer_interval    = var.kinesis_firehose_buffer_interval
+    compression_format = var.s3_compression_format
   }
 
   splunk_configuration {
-    hec_endpoint               = "${ var.hec_url }"
-    hec_token                  = "${ data.aws_kms_secrets.splunk_hec_token.plaintext["hec_token"] }"
-    hec_acknowledgment_timeout = "${ var.hec_acknowledgment_timeout }"
-    hec_endpoint_type          = "${ var.hec_endpoint_type }"
-    s3_backup_mode             = "${ var.s3_backup_mode }"
+    hec_endpoint               = var.hec_url
+    hec_token                  = data.aws_kms_secrets.splunk_hec_token.plaintext["hec_token"]
+    hec_acknowledgment_timeout = var.hec_acknowledgment_timeout
+    hec_endpoint_type          = var.hec_endpoint_type
+    s3_backup_mode             = var.s3_backup_mode
 
-    processing_configuration = [
-      {
-        enabled = "true"
+    processing_configuration {
+      enabled = "true"
 
-        processors = [
-          {
-            type = "Lambda"
+      processors {
+        type = "Lambda"
 
-            parameters = [
-              {
-                parameter_name  = "LambdaArn"
-                parameter_value = "${ aws_lambda_function.firehose_lambda_transform.arn }:$LATEST"
-              },
-              {
-                parameter_name  = "RoleArn"
-                parameter_value = "${ aws_iam_role.kinesis_firehose.arn }"
-              },
-            ]
-          },
-        ]
-      },
-    ]
+        parameters {
+          parameter_name  = "LambdaArn"
+          parameter_value = "${aws_lambda_function.firehose_lambda_transform.arn}:$LATEST"
+        }
+        parameters {
+          parameter_name  = "RoleArn"
+          parameter_value = aws_iam_role.kinesis_firehose.arn
+        }
+      }
+    }
 
     cloudwatch_logging_options {
-      enabled         = "${ var.enable_fh_cloudwatch_logging }"
-      log_group_name  = "${ aws_cloudwatch_log_group.kinesis_logs.name }"
-      log_stream_name = "${ aws_cloudwatch_log_stream.kinesis_logs.name }"
+      enabled         = var.enable_fh_cloudwatch_logging
+      log_group_name  = aws_cloudwatch_log_group.kinesis_logs.name
+      log_stream_name = aws_cloudwatch_log_stream.kinesis_logs.name
     }
   }
 
-  tags = "${ var.tags }"
+  tags = var.tags
 }
 
 # S3 Bucket for Kinesis Firehose s3_backup_mode
 resource "aws_s3_bucket" "kinesis_firehose_s3_bucket" {
-  bucket = "${ var.s3_bucket_name }"
-  region = "${ var.region }"
+  bucket = var.s3_bucket_name
+  region = var.region
   acl    = "private"
 
   server_side_encryption_configuration {
@@ -67,36 +61,36 @@ resource "aws_s3_bucket" "kinesis_firehose_s3_bucket" {
     }
   }
 
-  tags = "${ var.tags }"
+  tags = var.tags
 }
 
 # Cloudwatch logging group for Kinesis Firehose
 resource "aws_cloudwatch_log_group" "kinesis_logs" {
   name              = "/aws/kinesisfirehose/${var.firehose_name}"
-  retention_in_days = "${ var.cloudwatch_log_retention }"
+  retention_in_days = var.cloudwatch_log_retention
 
-  tags = "${ var.tags }"
+  tags = var.tags
 }
 
 # Create the stream
 resource "aws_cloudwatch_log_stream" "kinesis_logs" {
-  name           = "${ var.log_stream_name }"
-  log_group_name = "${ aws_cloudwatch_log_group.kinesis_logs.name }"
+  name           = var.log_stream_name
+  log_group_name = aws_cloudwatch_log_group.kinesis_logs.name
 }
 
 # handle the sensitivity of the hec_token variable
 data "aws_kms_secrets" "splunk_hec_token" {
   secret {
     name    = "hec_token"
-    payload = "${ var.hec_token }"
+    payload = var.hec_token
 
-    context = "${ var.encryption_context }"
+    context = var.encryption_context
   }
 }
 
 # Role for the transformation Lambda function attached to the kinesis stream
 resource "aws_iam_role" "kinesis_firehose_lambda" {
-  name        = "${ var.kinesis_firehose_lambda_role_name }"
+  name        = var.kinesis_firehose_lambda_role_name
   description = "Role for Lambda function to transformation CloudWatch logs into Splunk compatible format"
 
   assume_role_policy = <<POLICY
@@ -114,7 +108,8 @@ resource "aws_iam_role" "kinesis_firehose_lambda" {
 }
 POLICY
 
-  tags = "${ var.tags }"
+
+  tags = var.tags
 }
 
 data "aws_iam_policy_document" "lambda_policy_doc" {
@@ -124,7 +119,7 @@ data "aws_iam_policy_document" "lambda_policy_doc" {
     ]
 
     resources = [
-      "${ var.arn_cloudwatch_logs_to_ship }",
+      var.arn_cloudwatch_logs_to_ship,
     ]
 
     effect = "Allow"
@@ -136,7 +131,7 @@ data "aws_iam_policy_document" "lambda_policy_doc" {
     ]
 
     resources = [
-      "${ aws_kinesis_firehose_delivery_stream.kinesis_firehose.arn }",
+      aws_kinesis_firehose_delivery_stream.kinesis_firehose.arn,
     ]
   }
 
@@ -178,41 +173,41 @@ data "aws_iam_policy_document" "lambda_policy_doc" {
 }
 
 resource "aws_iam_policy" "lambda_transform_policy" {
-  name   = "${ var.lambda_iam_policy_name }"
-  policy = "${ data.aws_iam_policy_document.lambda_policy_doc.json }"
+  name   = var.lambda_iam_policy_name
+  policy = data.aws_iam_policy_document.lambda_policy_doc.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_role_attachment" {
-  role       = "${ aws_iam_role.kinesis_firehose_lambda.name }"
-  policy_arn = "${ aws_iam_policy.lambda_transform_policy.arn }"
+  role       = aws_iam_role.kinesis_firehose_lambda.name
+  policy_arn = aws_iam_policy.lambda_transform_policy.arn
 }
 
 # Create the lambda function
 # The lambda function to transform data from compressed format in Cloudwatch to something Splunk can handle (uncompressed)
 resource "aws_lambda_function" "firehose_lambda_transform" {
-  function_name    = "${ var.lambda_function_name }"
+  function_name    = var.lambda_function_name
   description      = "Transform data from CloudWatch format to Splunk compatible format"
-  filename         = "${ data.archive_file.lambda_function.output_path }"
-  role             = "${ aws_iam_role.kinesis_firehose_lambda.arn }"
+  filename         = data.archive_file.lambda_function.output_path
+  role             = aws_iam_role.kinesis_firehose_lambda.arn
   handler          = "kinesis-firehose-cloudwatch-logs-processor.handler"
-  source_code_hash = "${ data.archive_file.lambda_function.output_base64sha256 }"
-  runtime          = "${ var.nodejs_runtime }"
-  timeout          = "${ var.lambda_function_timeout }"
+  source_code_hash = data.archive_file.lambda_function.output_base64sha256
+  runtime          = var.nodejs_runtime
+  timeout          = var.lambda_function_timeout
 
-  tags = "${ var.tags }"
+  tags = var.tags
 }
 
 # kinesis-firehose-cloudwatch-logs-processor.js was taken by copy/paste from the AWS UI.  It is predefined blueprint
 # code supplied to AWS by Splunk.
 data "archive_file" "lambda_function" {
   type        = "zip"
-  source_file = "${ path.module }/files/kinesis-firehose-cloudwatch-logs-processor.js"
-  output_path = "${ path.module }/files/kinesis-firehose-cloudwatch-logs-processor.zip"
+  source_file = "${path.module}/files/kinesis-firehose-cloudwatch-logs-processor.js"
+  output_path = "${path.module}/files/kinesis-firehose-cloudwatch-logs-processor.zip"
 }
 
 # Role for Kenisis Firehose
 resource "aws_iam_role" "kinesis_firehose" {
-  name        = "${ var.kinesis_firehose_role_name }"
+  name        = var.kinesis_firehose_role_name
   description = "IAM Role for Kenisis Firehose"
 
   assume_role_policy = <<POLICY
@@ -230,7 +225,8 @@ resource "aws_iam_role" "kinesis_firehose" {
 }
 POLICY
 
-  tags = "${ var.tags }"
+
+  tags = var.tags
 }
 
 data "aws_iam_policy_document" "kinesis_firehose_policy_document" {
@@ -245,8 +241,8 @@ data "aws_iam_policy_document" "kinesis_firehose_policy_document" {
     ]
 
     resources = [
-      "${ aws_s3_bucket.kinesis_firehose_s3_bucket.arn }",
-      "${ aws_s3_bucket.kinesis_firehose_s3_bucket.arn }/*",
+      aws_s3_bucket.kinesis_firehose_s3_bucket.arn,
+      "${aws_s3_bucket.kinesis_firehose_s3_bucket.arn}/*",
     ]
 
     effect = "Allow"
@@ -259,7 +255,7 @@ data "aws_iam_policy_document" "kinesis_firehose_policy_document" {
     ]
 
     resources = [
-      "${ aws_lambda_function.firehose_lambda_transform.arn }:$LATEST",
+      "${aws_lambda_function.firehose_lambda_transform.arn}:$LATEST",
     ]
   }
 
@@ -269,8 +265,8 @@ data "aws_iam_policy_document" "kinesis_firehose_policy_document" {
     ]
 
     resources = [
-      "${ aws_cloudwatch_log_group.kinesis_logs.arn }",
-      "${ aws_cloudwatch_log_stream.kinesis_logs.arn }",
+      aws_cloudwatch_log_group.kinesis_logs.arn,
+      aws_cloudwatch_log_stream.kinesis_logs.arn,
     ]
 
     effect = "Allow"
@@ -278,17 +274,17 @@ data "aws_iam_policy_document" "kinesis_firehose_policy_document" {
 }
 
 resource "aws_iam_policy" "kinesis_firehose_iam_policy" {
-  name   = "${ var.kinesis_firehose_iam_policy_name }"
-  policy = "${ data.aws_iam_policy_document.kinesis_firehose_policy_document.json }"
+  name   = var.kinesis_firehose_iam_policy_name
+  policy = data.aws_iam_policy_document.kinesis_firehose_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "kenisis_fh_role_attachment" {
-  role       = "${ aws_iam_role.kinesis_firehose.name }"
-  policy_arn = "${ aws_iam_policy.kinesis_firehose_iam_policy.arn }"
+  role       = aws_iam_role.kinesis_firehose.name
+  policy_arn = aws_iam_policy.kinesis_firehose_iam_policy.arn
 }
 
 resource "aws_iam_role" "cloudwatch_to_firehose_trust" {
-  name        = "${ var.cloudwatch_to_firehose_trust_iam_role_name }"
+  name        = var.cloudwatch_to_firehose_trust_iam_role_name
   description = "Role for CloudWatch Log Group subscription"
 
   assume_role_policy = <<ROLE
@@ -305,6 +301,7 @@ resource "aws_iam_role" "cloudwatch_to_firehose_trust" {
   "Version": "2012-10-17"
 }
 ROLE
+
 }
 
 data "aws_iam_policy_document" "cloudwatch_to_fh_access_policy" {
@@ -316,7 +313,7 @@ data "aws_iam_policy_document" "cloudwatch_to_fh_access_policy" {
     effect = "Allow"
 
     resources = [
-      "${ aws_kinesis_firehose_delivery_stream.kinesis_firehose.arn }",
+      aws_kinesis_firehose_delivery_stream.kinesis_firehose.arn,
     ]
   }
 
@@ -328,26 +325,27 @@ data "aws_iam_policy_document" "cloudwatch_to_fh_access_policy" {
     effect = "Allow"
 
     resources = [
-      "${ aws_iam_role.cloudwatch_to_firehose_trust.arn }",
+      aws_iam_role.cloudwatch_to_firehose_trust.arn,
     ]
   }
 }
 
 resource "aws_iam_policy" "cloudwatch_to_fh_access_policy" {
-  name        = "${ var.cloudwatch_to_fh_access_policy_name }"
+  name        = var.cloudwatch_to_fh_access_policy_name
   description = "Cloudwatch to Firehose Subscription Policy"
-  policy      = "${ data.aws_iam_policy_document.cloudwatch_to_fh_access_policy.json }"
+  policy      = data.aws_iam_policy_document.cloudwatch_to_fh_access_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "cloudwatch_to_fh" {
-  role       = "${ aws_iam_role.cloudwatch_to_firehose_trust.name }"
-  policy_arn = "${ aws_iam_policy.cloudwatch_to_fh_access_policy.arn }"
+  role       = aws_iam_role.cloudwatch_to_firehose_trust.name
+  policy_arn = aws_iam_policy.cloudwatch_to_fh_access_policy.arn
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_filter" {
-  name            = "${ var.cloudwatch_log_filter_name }"
-  role_arn        = "${ aws_iam_role.cloudwatch_to_firehose_trust.arn }"
-  destination_arn = "${ aws_kinesis_firehose_delivery_stream.kinesis_firehose.arn }"
-  log_group_name  = "${ var.name_cloudwatch_logs_to_ship }"
-  filter_pattern  = "${ var.subscription_filter_pattern }"
+  name            = var.cloudwatch_log_filter_name
+  role_arn        = aws_iam_role.cloudwatch_to_firehose_trust.arn
+  destination_arn = aws_kinesis_firehose_delivery_stream.kinesis_firehose.arn
+  log_group_name  = var.name_cloudwatch_logs_to_ship
+  filter_pattern  = var.subscription_filter_pattern
 }
+
