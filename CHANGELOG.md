2f5699f2756e994250099a339557ff3df3b1be41
Mitchell Cooper <mitchell.l.cooper@disney.com>
2020-03-26 14:36:53 -0400

    Pinned to ~> 0.12.0 of terraform; TF13 may have introduced a breaking change

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Change Log for Terraform AWS Kinesis Firehose Splunk
 
+## 1.0.0 - Breaking Change
+  * Upgraded for Terraform 12 compatibility (thanks [kevinkuszyk](https://github.com/kevinkuszyk))
+  * Added git ignore file
+
 ## 0.1.0
   * Initial release
--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,7 @@
-# This code was originally developed with Terraform v0.11.11
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 0.12.0"
 }
 
-# This code was originally developed with AWS provider v1.55
 provider "aws" {
   version = "~> 2.7"
 
