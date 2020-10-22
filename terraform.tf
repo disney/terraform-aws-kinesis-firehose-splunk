bf4f26cec4f377d63918362b564da6cf1406ed99
Alex Farhadi <alex.farhadi@clearme.com>
2020-10-22 15:08:12 -0500

    Adjust the module to be able to support for TF 13 but also lock so that new versions to not break things

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 0.12.0"
+  required_version = ">= 0.12.0, < 0.14.0"
 }
 
 provider "aws" {
-  version = "~> 2.7"
+  version = ">= 3.0, <= 3.11.0"
 
   region = var.region
 }
