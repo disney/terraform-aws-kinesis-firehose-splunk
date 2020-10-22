bd78283d047dcbd3614c080a9365ab0743f3acc9
Alex Farhadi <alex.farhadi@clearme.com>
2020-10-22 16:14:33 -0500

    Less restrictive version locking

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.12.0, < 0.14.0"
+  required_version = ">= 0.12.0"
 }
 
 provider "aws" {
-  version = ">= 2.7.0, <= 3.11.0"
+  version = ">= 2.7.0"
 
   region = var.region
 }
