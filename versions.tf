41816231947f18586fc256c9c73ff21616785ba5
Mitchell Cooper <Mitchell.L.Cooper@disney.com>
2023-03-14 14:04:10 -0400

    fix formatting

--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">= 4.0.0, < 5.0.0"
     }
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
       version = ">= 2.3.0, < 3.0.0"
     }
   }
